fb7b04b044803497a1e741b898c4fc2b2ad15b9b
devsrabon <et.srabon@gmail.com>
2022-10-26 08:21:03 +0600

    added login page

--- src/Pages/LogIn/LogIn.js
+++ src/Pages/LogIn/LogIn.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 const LogIn = () => {
     return (
-        <div>
-            <h2>THis is login</h2>
-            <Link to='/register'>register</Link>
-        </div>
-    );
+			<div className="container">
+				<Form>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>Email address</Form.Label>
+						<Form.Control
+							type="email"
+							name="email"
+							required
+							placeholder="Enter email"
+						/>
+					</Form.Group>
+					<Form.Group className="mb-3" controlId="formBasicPassword">
+						<Form.Label>Password</Form.Label>
+						<Form.Control
+							type="password"
+							name="password"
+							required
+							placeholder="Password"
+						/>
+					</Form.Group>
+					<Button variant="success" type="submit">
+						Submit
+					</Button>
+				</Form>
+				<>
+					New here? please <Link to="/register">Register</Link>
+				</>
+			</div>
+		);
 };
 
 export default LogIn;
