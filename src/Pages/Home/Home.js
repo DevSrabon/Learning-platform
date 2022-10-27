adfafcbd047ff20ee9de28a7c1e985b0307da3ec
devsrabon <et.srabon@gmail.com>
2022-10-27 22:21:23 +0600

    added Home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button';
+import './Home.css'
+import { Link } from 'react-router-dom';
 const Home = () => {
     return (
-        <div>
-            <h2>This is home</h2>
-        </div>
-    );
+			<div className="body d-flex flex-column text-white align-items-center justify-content-center">
+				<h2>Welcome to Programming School</h2>
+				<h6>Anyone can learn Programming Language from Here!</h6>
+				<Link to="/courses">
+					<Button variant="outline-secondary">Get Started</Button>
+				</Link>
+			</div>
+		);
 };
 
 export default Home;
